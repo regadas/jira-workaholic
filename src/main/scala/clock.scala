f3c41d369c1d576a6978d2afa7b35824834d7971
regadas <filiperegadas@gmail.com>
2012-02-21 19:24:56 +0000

    logger declared implictly for clock

--- src/main/scala/clock.scala
+++ src/main/scala/clock.scala
@@ -3,11 +3,11 @@ import com.weiglewilczek.slf4s.Logger
 
 object Clock {
   import System.{ currentTimeMillis => now }
-  def apply[T](what: String, l: Logger)(f: => T) = {
+  def apply[T](what: String)(f: => T)(implicit logger: Logger) = {
     val then = now
     try { f }
     finally {
-      l.debug("%s took %d ms" format (what, now - then))
+      logger.debug("%s took %d ms" format (what, now - then))
     }
   }
 }
--- src/main/scala/jira/client.scala
+++ src/main/scala/jira/client.scala
@@ -4,7 +4,7 @@ import eu.regadas._
 import model._
 import java.net.URL
 import com.atlassian.jira.rpc.soap.client._
-import com.weiglewilczek.slf4s.Logging
+import com.weiglewilczek.slf4s.{ Logger, Logging }
 
 abstract class Client(val service: JiraSoapService) {
   def this(url: URL) = this((new JiraSoapServiceServiceLocator).getJirasoapserviceV2(url))
@@ -12,36 +12,40 @@ abstract class Client(val service: JiraSoapService) {
 
 case class Api(url: URL) extends Client(url) with Logging {
 
-  def login(user: String, password: String) = Clock("jira auth", logger) {
+  implicit override lazy val logger: Logger = logger
+
+  def login(user: String, password: String) = Clock("jira auth") {
     new ClientToken(service.login(user, password), user)
   }
 
-  def user(auth: ClientToken) = Clock("jira user info", logger) {
+  def logout = Clock
+
+  def user(auth: ClientToken) = Clock("jira user info") {
     service.getUser(auth.token, auth.user)
   }
 
   object project {
-    def list(auth: ClientToken): List[Project] = Clock("jira project list", logger) {
+    def list(auth: ClientToken): List[Project] = Clock("jira project list") {
       service.getProjectsNoSchemes(auth.token).toList
     }
 
-    def worklogs(auth: ClientToken, project: String, max: Option[Int] = None): List[WorkLog] = Clock("jira projects worklogs", logger) {
+    def worklogs(auth: ClientToken, project: String, max: Option[Int] = None): List[WorkLog] = Clock("jira projects worklogs") {
       issue.list(auth, project, max) flatMap { i => issue.worklogs(auth, project, i.key) }
     }
   }
 
   object issue {
-    def list(auth: ClientToken, project: String, max: Option[Int] = None): List[Issue] = Clock("jira issue list", logger){
+    def list(auth: ClientToken, project: String, max: Option[Int] = None): List[Issue] = Clock("jira issue list") {
       service.getIssuesFromJqlSearch(auth.token, "project = \"%s\" AND assignee = \"%s\"" format (project, auth.user), max.getOrElse(20)).toList
     }
 
-    def worklogs(auth: ClientToken, project: String, issue: String): List[WorkLog] = Clock("jira issue worklog", logger){
+    def worklogs(auth: ClientToken, project: String, issue: String): List[WorkLog] = Clock("jira issue worklog") {
       (service.getWorklogs(auth.token, issue) filter { wl => wl.getAuthor == auth.user } toList) map { remote => WorkLog(project, issue, remote) }
     }
   }
-  
+
   object worklog {
-    def add(auth: ClientToken, issue: String, worklog: WorkLog) = Clock("jira add worklog", logger) {
+    def add(auth: ClientToken, issue: String, worklog: WorkLog) = Clock("jira add worklog") {
       service.addWorklogAndAutoAdjustRemainingEstimate(auth.token, issue, worklog)
     }
   }
